89a99aeaf6d395762c6664960bdd238d267603cc
CiachoG <1055309935@qq.com>
2020-05-24 22:57:20 +0800

    lru

---
+++ src/lru/lru.go
@@ -0,0 +1,85 @@
+/**
+* @Author: CiachoG
+* @Date: 2020/5/23 15:22
+* @Description：
+ */
+package lru
+
+import (
+	"container/list"
+)
+
+//day1：非并发安全的cache结构体
+type Cache struct {
+	maxBytes  int64      //最大内存
+	nBytes    int64      //已使用的内存
+	ll        *list.List //双向链表，使用标准库
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value) //记录移除时的回调函数
+}
+
+type entry struct { //list的节点，保存key的目的是方便删除对应的映射
+	key   string
+	value Value
+}
+
+//Value interface
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+//查找
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele) //双向链表队首队尾是相对的，约定front为队尾
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+//删除
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nBytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+//add
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nBytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nBytes += int64(len(key)) + int64(value.Len())
+	}
+	//delete
+	for c.maxBytes != 0 && c.maxBytes < c.nBytes {
+		c.RemoveOldest()
+	}
+}
+
+//Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
---
+++ src/lru/lru_test.go
@@ -0,0 +1,43 @@
+package lru
+
+import "testing"
+
+type String string
+
+//implement the interface of Value
+func (s String) Len() int {
+	return len(s)
+}
+
+func TestCache_Get(t *testing.T) {
+	lru := New(int64(0), nil)
+	lru.Add("k1", String("1234"))
+	t.Log(lru.cache["k1"].Value)
+	if v, ok := lru.Get("k1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit k1 = 1234 failed")
+	} else {
+		t.Log("the value of k1 is ", v)
+	}
+	if _, ok := lru.Get("k2"); ok {
+		t.Fatalf("cache miss k2 failed")
+	}
+
+}
+func TestCache_RemoveOldest(t *testing.T) {
+	k1, k2, k3 := "k1", "k2", "k3"
+	v1, v2, v3 := "value1", "value2", "value3"
+	cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+	t.Log("len of lru", lru.nBytes)
+	if _, ok := lru.Get("k1"); ok {
+		t.Fatalf("remove k1 failed")
+	}
+
+}
+
+func TestOnEvicted(t *testing.T) {
+
+}
